ba98039b96ab7c37c34ae5c732bb56d2d36b2c6c
DanielHall-Eardley <350chevy8@gmail.com>
2020-07-14 17:35:46 -0400

    added deployment url

--- src/global.js
+++ src/global.js
@@ -1,2 +1,3 @@
-export const apiHost = 'http://localhost:3000'
+// export const apiHost = 'http://localhost:3000'
+export const apiHost = 'https://greve-chocolatine-96579.herokuapp.com'
 export const stripeApiKey = 'pk_test_Bo8TQrRb1VZuEd1bzgFJXSqC00ofaJvGQu'
